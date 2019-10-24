36584d16084a9984cacdb939763b8cf1bbf57be1
elsanderson <e-vansanderson@hotmail.com>
2019-10-23 19:55:54 -0700

    Update routetests.ts

--- GradeAchiever/src/__tests__/routetests.ts
+++ GradeAchiever/src/__tests__/routetests.ts
@@ -1,27 +1,27 @@
 import { BaseRoute } from "./routes/route";
 import {Request, Response } from "express";
 
-testBaseRouteInit{
+testBaseRouteInit({
 	  let testBaseRoute:BaseRoute = new BaseRoute();
   	expect(testBaseRoute.gettitle() == "My New TypeScript Web app");
-}
+})
 
-testBaseRouteAddScript{
+testBaseRouteAddScript({
   let testBaseRoute:BaseRoute = new BaseRoute();
   testBaseRoute = testBaseRoute.addScript("The Sky is Blue");
   expect(testBaseRoute.getScripts()== "The Sky is Blue");
-  }
+})
 
- testBaseRouteRender{
+ testBaseRouteRender({
   let testRender:BaseRoute = new BaseRoute();
   testRender = testBaseRoute.addScript("The sky is purple");
   testRender.render(req, res, "userhome", options);
   expect(res.locals.BASE_URL == "/");
   expect(res.locals.scripts == testRender.scripts);
   expect(res.locals.title == testRender.title);
-}
+})
 
-testBaseRouteAddScriptFAIL{
+testBaseRouteAddScriptFAIL({
   let testBaseRoute:BaseRoute = new BaseRoute();
   let ans = 2;
 try {
@@ -31,4 +31,4 @@ catch(error) {
   ans = 3;
 }
 expect(ans==3);
-}
+})
