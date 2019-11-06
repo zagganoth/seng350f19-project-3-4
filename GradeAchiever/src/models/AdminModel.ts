838a302ad29d4368c0aa8e8d00c33626b65fdfbd
Reed McIlwain <reed.mcilwain@hotmail.com>
2019-11-06 06:32:51 -0800

    Some clean up in the admin model, removing old code

--- GradeAchiever/src/models/AdminModel.ts
+++ GradeAchiever/src/models/AdminModel.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import DbClient = require("../DbClient");
-import {BaseModel} from "./BaseModel";
+import { BaseModel } from "./BaseModel";
 
 export class AdminModel extends BaseModel {
     private readonly isAdmin: boolean;
@@ -16,7 +16,7 @@ export class AdminModel extends BaseModel {
         // Return the id and name for all students
         try {
             console.log("Admin Model - trying to delete one");
-            return await this.deleteOne({StudentID: userID});
+            return await this.deleteOne({ StudentID: userID });
         } catch (error) {
             console.log(error);
             console.log("error from deleteOne user");
@@ -28,7 +28,7 @@ export class AdminModel extends BaseModel {
     public async GetAllUsers() {
         // Return the id and name for all students
         try {
-            return await this.getAll({}, {StudentID: 1, StudentName: 1});
+            return this.getAll({}, { StudentID: 1, StudentName: 1 });
         } catch (error) {
             console.log(error);
             console.log("error from getall users");
@@ -38,38 +38,25 @@ export class AdminModel extends BaseModel {
 
     // Adds a new user
     public async AddUser(userID: number) {
-        if (this.isAdmin) {
-            console.log("Deleting user id " + userID + " from db.");
-            return await DbClient.connect()
-            .then((db) => {
-                /*TODO stuff to add new user to DB
-                 currently returns users that are left*/
-                return db.collection(this.tableName).find().project({StudentID: 1, StudentName: 1}).toArray();
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-        } else {
-            throw new Error("Invalid admin credentials");
+        try {
+            console.log("Admin Model - adding a user");
+            return this.addOne({});
+        } catch (error) {
+            console.log(error);
+            console.log("error from AddUser users");
+            return [];
         }
     }
 
     // Gets user by ID and updates user details
     public async UpdateUser(userID: number, update: object) {
-        if (this.isAdmin) {
-            console.log("Deleting user id " + userID + " from db.");
-            return await DbClient.connect()
-            .then((db) => {
-                /*TODO stuff to update user
-                 currently returns users that are left*/
-                return db.collection(this.tableName).find().project({StudentID: 1, StudentName: 1}).toArray();
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-        } else {
-            throw new Error("Invalid admin credentials");
+        try {
+            console.log("Admin Model - Updating a user")
+            return this.editOne({StudentID: userID}, update);
+        } catch (error) {
+            console.log(error);
+            console.log("error from UpdateUser users");
+            return [];
         }
     }
-
 }
