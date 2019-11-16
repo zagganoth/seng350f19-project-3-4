66d2bb11157dad8fdb054da03c6030ef3852e586
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-11-16 14:43:06 -0800

    Updated formatting

--- GradeAchiever/src/__tests__/controllers/SessionControllertest.ts
+++ GradeAchiever/src/__tests__/controllers/SessionControllertest.ts
@@ -13,14 +13,29 @@ describe("Session Controller Tests", () => {
         controller = new SessionController();
     });
 
-    it("should get all users", async () => {
-        return controller.RequestUsers(req, res, next).then((users: any) => {
+    it("requests get all users", async () => {
+        return controller.RequestUsers(req, res, next)
+        .then((users: any) => {
             expect(users.length).toEqual(3);
         });
     });
+
     it("no functionality yet", async () => {
-        return controller.VerifyUser(req, res, next, 1).then((users: any) => {
+        return controller.VerifyUser(req, res, next, 1)
+        .then((users: any) => {
             expect(1);
         });
     });
+
+    /*
+    it("Creates a new user", async () => {
+        const name = "Test User";
+        const email = "test@email.com";
+        return controller.CreateUser(req, res, next, name, email)
+        .then((returnVal: any) => {
+            expect(returnVal.insertedCount).toEqual(1);
+        });
+    });
+    */
+
 });
