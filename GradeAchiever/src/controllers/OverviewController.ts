3d0a77c33afd273b7436d6b2d55867edfdffc6b3
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-11-16 15:25:28 -0800

    Refactored to reduce method size

--- GradeAchiever/src/controllers/OverviewController.ts
+++ GradeAchiever/src/controllers/OverviewController.ts
@@ -1,8 +1,5 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { CourseController } from "../controllers/CourseController";
-import {AdminModel} from "../models/AdminModel";
-// import {CourseModel} from "../models/CourseModel";
-import {GradableItemModel} from "../models/GradableItemModel";
 import { UserModel } from "../models/UserModel";
 
 export class OverviewController {
@@ -12,46 +9,51 @@ export class OverviewController {
 
     // Gets user by ID and their courses, gradable items (in sorted order)
     public async RequestUser(id: number) {
-
         const um = new UserModel(id);
+        const CourseCtrl = new CourseController();
+        const retVal = [];
         // Get all details for the user, including courses
         const userDetails = await um.GetUserDetails(id);
-        // Prepare to query details from both the courses and the models
-        // const gm = new GradableItemModel();
-
         // gets course and grade info from courseController
-        const CourseCtrl = new CourseController();
-
         const gradableItems = [];
         const courses = [];
         if ("Courses" in  userDetails && userDetails.Courses !== []) {
-            let courseDetails: any;
-            let gradableItemsbyCourse: any;
             // For each course
             for (const course of userDetails.Courses) {
                 // Get the course details, including all gradable items
-                courseDetails = await CourseCtrl.RequestCourse(course);
-                console.log("**** **** **** Course Details: " + courseDetails);
-                // get gradable items by course id from Course Controller
-                gradableItemsbyCourse = await CourseCtrl.RequestCourseGradableItems(course);
-                console.log("Gradable Items by course:" + gradableItemsbyCourse);
-                for (const gradableitem of gradableItemsbyCourse) {
-                    // Add course name to each gradable item
-                    gradableitem.CourseName = courseDetails.CourseName;
-                    gradableItems.push(gradableitem);
-                }
+                const courseDetails = await CourseCtrl.RequestCourse(course);
                 courses.push(courseDetails);
+                for(const item of await this.RequestGradableItems(course, (courseDetails as any).CourseName)){
+                    gradableItems.push(item);
+                }
             }
-
         }
         // Sort gradable items by due date
-        gradableItems.sort((a, b) => a.DueDate < b.DueDate ? -1 : a.DueDate > b.DueDate ? 1 : 0);
-        console.log(gradableItems);
-        const retVal = [];
+        gradableItems.sort((a, b) => (a as any).DueDate < (b as any).DueDate ? -1 : (a as any).DueDate > (b as any).DueDate ? 1 : 0);
         retVal.push(userDetails);
         retVal.push(courses);
         retVal.push(gradableItems);
         return retVal;
-
     }
+
+
+    /**
+     * Requests all gradable items by course and sorts in order by duedate
+     */
+    public async RequestGradableItems(courseID: number, courseName: string) {
+        const CourseCtrl = new CourseController();
+        // get gradable items by course id from Course Controller
+        let gradableItems = [];
+        const gradableItemsbyCourse = await CourseCtrl.RequestCourseGradableItems(courseID);
+        if (gradableItemsbyCourse){
+            for (const gradableitem of gradableItemsbyCourse) {
+                // Add course name to each gradable item
+                (gradableitem as any).CourseName = courseName;
+                gradableItems.push(gradableitem);
+            }
+        }
+        return gradableItems;
+    }
+
+
 }
