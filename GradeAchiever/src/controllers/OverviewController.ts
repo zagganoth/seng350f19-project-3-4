3bb9e4d1208633d3cd93cac796cb9bc62d715169
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-11-17 11:43:14 -0800

    Refactored into smaller methods and fixed bug

--- GradeAchiever/src/controllers/OverviewController.ts
+++ GradeAchiever/src/controllers/OverviewController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { CourseController } from "../controllers/CourseController";
 import { UserModel } from "../models/UserModel";
+import { GradableItemModel } from "../models/GradableItemModel";
 
 export class OverviewController {
     constructor() {
@@ -15,7 +16,7 @@ export class OverviewController {
         // Get all details for the user, including courses
         const userDetails = await um.GetUserDetails(id);
         // gets course and grade info from courseController
-        const gradableItems = [];
+        let gradableItems: Array<GradableItemModel> = [];
         const courses = [];
         if ("Courses" in  userDetails && userDetails.Courses !== []) {
             // For each course
@@ -23,13 +24,11 @@ export class OverviewController {
                 // Get the course details, including all gradable items
                 const courseDetails = await CourseCtrl.RequestCourse(course);
                 courses.push(courseDetails);
-                for (const item of await this.RequestGradableItems(course, (courseDetails as any).CourseName)) {
-                    gradableItems.push(item);
-                }
+                gradableItems = await this.RequestGradableItems(course, (courseDetails as any).CourseName, gradableItems);
             }
         }
         // Sort gradable items by due date
-        gradableItems.sort((a, b) => (a as any).DueDate < (b as any).DueDate ? -1 : (a as any).DueDate > (b as any).DueDate ? 1 : 0);
+        //gradableItems.sort((a:any, b:any) => a.DueDate < b.DueDate ? -1 : a.DueDate > b.DueDate ? 1 : 0);
         retVal.push(userDetails);
         retVal.push(courses);
         retVal.push(gradableItems);
@@ -39,18 +38,17 @@ export class OverviewController {
     /**
      * Requests all gradable items by course and sorts in order by duedate
      */
-    public async RequestGradableItems(courseID: number, courseName: string) {
+    public async RequestGradableItems(courseID: number, courseName: string, gradableItems: Array<GradableItemModel>) {
         const CourseCtrl = new CourseController();
         // get gradable items by course id from Course Controller
-        const gradableItems = [];
-        const gradableItemsbyCourse = await CourseCtrl.RequestCourseGradableItems(courseID);
-        if (gradableItemsbyCourse) {
-            for (const gradableitem of gradableItemsbyCourse) {
-                // Add course name to each gradable item
-                (gradableitem as any).CourseName = courseName;
-                gradableItems.push(gradableitem);
-            }
+        const gradableItemsbyCourse: any = await CourseCtrl.RequestCourseGradableItems(courseID);
+        for (const gradableitem of gradableItemsbyCourse) {
+            // Add course name to each gradable item
+            gradableitem.CourseName = courseName;
+            gradableItems.push(gradableitem);
         }
+        // Sort gradable items by due date
+        gradableItems.sort((a, b) => a.DueDate < b.DueDate ? -1 : a.DueDate > b.DueDate ? 1 : 0);
         return gradableItems;
     }
 
