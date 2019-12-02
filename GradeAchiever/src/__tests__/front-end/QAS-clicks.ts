5d0650219999f2a1fe09157b00923b32ff70e795
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-12-01 16:11:23 -0800

    Added two tests for logging in

--- GradeAchiever/src/__tests__/front-end/QAS-clicks.ts
+++ GradeAchiever/src/__tests__/front-end/QAS-clicks.ts
@@ -2,14 +2,39 @@
  * Tests for QAS CI testing. User input simplicity and efficiency
  */
 
-describe("Google", () => {
-    beforeAll(async () => {
-      await page.goto("https://google.com");
+describe("Web App", () => {
+    const InputClickMax:number = 5;
+    beforeEach(async () => {
+        await page.goto("http://localhost:3000", {waitUntil:'load'});
     });
 
-    it('should display "google" text on page', async () => {
-      const text = await page.evaluate(() => document.body.textContent);
-      expect(text).toContain("google");
+    it('should login in < 5 clicks', async() => {
+        let clicks = 0;
+        await page.select("select[name='user']");
+        clicks +=1;
+        await page.waitForSelector("option");
+        await page.select("select[name='user']", '5');
+        clicks +=1;
+        await page.click("input[type='submit'][value='Login']");
+        clicks +=1;
+        await page.waitForSelector(".openBtn");
+        expect(page.url()).toContain("overview");
+        console.log(clicks);
+        expect(clicks).toBeLessThanOrEqual(InputClickMax);
+    });
+
+    it('should create a new user in < 5 clicks', async() => {
+        let clicks = 0;
+        await page.type("input[type='text'][name='name']", "testuser");
+        clicks +=1;
+        await page.type("input[type='text'][name='email']", "testuser@email.com");
+        clicks +=1;
+        await page.click("input[type='submit'][value='Create User']");
+        clicks +=1;
+        await page.waitForSelector(".openBtn");
+        expect(page.url()).toContain("newUser");
+        console.log(clicks);
+        expect(clicks).toBeLessThanOrEqual(InputClickMax);
     });
 
   });
