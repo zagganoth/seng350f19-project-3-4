5c736ca44119f285fa48ed42559c0bf9af5d5d18
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-12-02 13:34:21 -0800

    Added 3 tests

--- GradeAchiever/src/__tests__/front-end/QAS-clicks.ts
+++ GradeAchiever/src/__tests__/front-end/QAS-clicks.ts
@@ -2,9 +2,15 @@
  * Tests for QAS CI testing. User input simplicity and efficiency
  */
 
+jest.mock("../../models/CourseModel");
+jest.mock("../../models/GradableItemModel");
+jest.mock("../../models/AdminModel");
+jest.mock("../../models/UserModel");
+
+
 describe("Web App", () => {
     const InputClickMax: number = 5;
-    beforeEach(async () => {
+    beforeAll(async () => {
         await page.goto("http://localhost:3000", {waitUntil: "load"});
     });
 
@@ -19,11 +25,36 @@ describe("Web App", () => {
         clicks += 1;
         await page.waitForSelector(".openBtn");
         expect(page.url()).toContain("overview");
-        console.log(clicks);
         expect(clicks).toBeLessThanOrEqual(InputClickMax);
     });
 
+
+    it("should add new gradable item in < 5 clicks", async () => {
+        let clicks = 0;
+        //Select course home page
+        await page.click("input[value='SENG 350']");
+        await page.waitForSelector("#additembutton");
+        //select plus button
+        await page.click("#additembutton");
+        clicks += 1;
+        //wait for gradableitem overlay
+        await page.waitForSelector("#newgradableitem");
+        await page.type("input[name='GradableItems[0][name]']", "testitem");
+        clicks += 1;
+        await page.type("input[name='GradableItems[0][weight]']", "10");
+        clicks += 1;
+        await page.type("input[name='GradableItems[0][duedate]']", "10/09/2019");
+        clicks += 1;
+        await page.click("input[type='submit'][value='Submit']");
+        clicks += 1;
+        await page.waitForSelector(".course-details");
+        expect(page.url()).toContain("newGradableItem");
+        expect(clicks).toBeLessThanOrEqual(InputClickMax);
+    });
+
+
     it("should create a new user in < 5 clicks", async () => {
+        await page.goto("http://localhost:3000", {waitUntil: "load"});
         let clicks = 0;
         await page.type("input[type='text'][name='name']", "testuser");
         clicks += 1;
@@ -33,8 +64,33 @@ describe("Web App", () => {
         clicks += 1;
         await page.waitForSelector(".openBtn");
         expect(page.url()).toContain("newUser");
-        console.log(clicks);
         expect(clicks).toBeLessThanOrEqual(InputClickMax);
     });
+
+/*
+    it("should add new course in < 5 clicks using the pdf parse", async () => {
+        let clicks = 0;
+        //Select course home page
+        await page.click("input[value='SENG 350']");
+        await page.waitForSelector("#additembutton");
+        //select plus button
+        await page.click("#additembutton");
+        clicks += 1;
+        //wait for gradableitem overlay
+        await page.waitForSelector("#newgradableitem");
+        await page.type("input[name='GradableItems[0][name]']", "testitem");
+        clicks += 1;
+        await page.type("input[name='GradableItems[0][weight]']", "10");
+        clicks += 1;
+        await page.type("input[name='GradableItems[0][duedate]']", "10/09/2019");
+        clicks += 1;
+        await page.click("input[type='submit'][value='Submit']");
+        clicks += 1;
+        await page.waitForSelector(".course-details");
+        expect(page.url()).toContain("newGradableItem");
+        expect(clicks).toBeLessThanOrEqual(InputClickMax);
+    });*/
+
+
 
   });
