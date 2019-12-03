7bd004ab584935487e62a8e30e8b1b67eadbcfd7
elsanderson <e-vansanderson@hotmail.com>
2019-12-03 11:53:03 -0800

    should work

--- GradeAchiever/src/controllers/GradableItemController.ts
+++ GradeAchiever/src/controllers/GradableItemController.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { GradableItemModel } from "../models/GradableItemModel";
-=======
-import { GradableItemModel} from "../models/GradableItemModel";
 import { CourseModel }      from "../models/CourseModel";
 import { Algorithm }        from "../algorithm/Algorithm"
->>>>>>> 03756b8... Just need someone who understands how the models work
 
 export class GradableItemController {
     private gradableItemModel = new GradableItemModel();
@@ -50,7 +46,6 @@ export class GradableItemController {
             return [];
         });
     }
-<<<<<<< HEAD
 
     public async EditItemGrade(id: number, grade: number) {
         return this.gradableItemModel.EditGradableItemGrade(id, grade)
@@ -72,7 +67,8 @@ export class GradableItemController {
             console.log(error);
             return [];
         });
-=======
+    }
+
     public async EditGradableItem(id: number, name: string, duedate: Date, hours: number, grade: number) {
         const gradableItemModel = new GradableItemModel();
         try {
@@ -84,70 +80,7 @@ export class GradableItemController {
             console.log(error);
             return false;
         }
-        if (grade != 0) {
-          let gradableItem = await gradableItemModel.GetGradableItemDetails(id);
-          const courseID: any = gradableItem.CourseID;
-          const hoursRecommended = gradableItem.RecommendedTime;
-          const hoursSpent = gradableItem.StudiedTime;
-          const courseModel = new CourseModel();
-          const gradeGoalCourse = await courseModel.GetCourseDetails( courseID );
-          const gradeGoal = gradeGoalCourse.GradeGoal;
-          const algorithm = new Algorithm();
-          const itemRatio = algorithm.item_completed_calculation(gradeGoal, hoursSpent, hoursRecommended, grade);
-
-        try {
-          await gradableItemModel.AddGItemAccuracy(Number(id), Number(itemRatio));
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-        const gradableItems = gradeGoalCourse.GradableItems;
-        let i = 0;
-        let itemRatios = [];
-        let percentageWorth = [];
-        let percentageAchieved = [];
-        for (let val of gradableItems) {
-            gradableItem = await gradableItemModel.GetGradableItemDetails(val);
-            itemRatios[i] = gradableItem.GItemAccuracy;
-            percentageWorth[i] = gradableItem.Weight;
-            percentageAchieved[i] = gradableItem.CurrentGrade;
-        }
-        const courseCalResults = algorithm.course_calculation(itemRatios, percentageWorth, percentageAchieved, gradeGoal)
-        const courseRatio = courseCalResults[0];
-        const percentageDone = courseCalResults[1];
-        const newCourseGoal = courseCalResults[2];
-        const courseGrade = courseCalResults[3];
-        try {
-          await courseModel.EditCurrentGrade(Number(courseID), Number( courseGrade ));
-          await courseModel.EditGradeNeeded(Number(courseID), Number( newCourseGoal ));
-          await courseModel.EditPercentageDone(Number(courseID), Number( percentageDone ));
-          await courseModel.EditCourseRatio(Number(courseID), Number( courseRatio ));
-        }
-        catch (error) {
-            return false;
-        }
-        i = 0;
-        const difficulty = gradeGoalCourse.PerceivedDifficulty;
-        let weight = 0;
-        let itemHours = 0;
-        let itemID = 0;
-        for (let val of gradableItems) {
-          gradableItem = await gradableItemModel.GetGradableItemDetails(val);
-          weight = gradableItem.Weight;
-          if (weight>0) {
-            itemHours = algorithm.new_item_calculation(courseRatio, percentageDone, difficulty, newCourseGoal, weight);
-            itemID = gradableItem.GradableItemID;
-            try {
-              await gradableItemModel.AddRecommendedTime(Number(itemID), Number(itemHours));
-            }
-            catch (error) {
-                return false;
-            }
-          }
-        }
-      }
-        return true;
->>>>>>> 03756b8... Just need someone who understands how the models work
+        
     }
 
     // public async EditGradableItem(id: number, name: string, duedate: Date, hours: number, grade: number) {
