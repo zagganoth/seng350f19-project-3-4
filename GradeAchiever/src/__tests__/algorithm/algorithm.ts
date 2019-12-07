25fa0b8453fd5639d66b1f900959a2a4b8d9b334
elsanderson <43866659+elsanderson@users.noreply.github.com>
2019-12-06 19:28:54 -0800

    algorithm CI testing

--- GradeAchiever/src/__tests__/algorithm/algorithm.ts
+++ GradeAchiever/src/__tests__/algorithm/algorithm.ts
@@ -2,26 +2,38 @@ import { course_calculation, item_completed_calculation, new_item_calculation } 
 
 test("test item completed calculation", () => {
 	let itemRatio = 0;
+	const timerone = new Date();
+	const first = timerone.getSeconds();
 	itemRatio = item_completed_calculation(80, 8, 4, 40, 20);
+	const second = timerone.getSeconds();
 	expect(itemRatio === 4);
+	expect((second - first) < 2);
 	},
 );
 
 test("test course calculation", () => {
+	const timerone = new Date();
+	const first = timerone.getSeconds();
 	const itemRatio = [.5, 1.3, .7, 1.5];
 	const percentageWorth = [10, 10, 10, 10];
 	const percentageAchieved = [80, 80, 80, 80];
 	let testValue = [0, 0, 0];
 	testValue = course_calculation(itemRatio, percentageWorth, percentageAchieved, 80);
+	const second = timerone.getSeconds();
 	expect(testValue[0] === 1);
 	expect(testValue[1] === 40);
 	expect(testValue[2] === 80);
+	expect((second - first) < 2);
 	},
 );
 
 test("new item calculation easy", () => {
 	let testValue = 0;
+	const timerone = new Date();
+	const first = timerone.getSeconds();
 	testValue = new_item_calculation(1, 20, 3, 80, 10);
+	const second = timerone.getSeconds();
 	expect(testValue === 5);
+	expect((second - first) < 2);
 	},
 );
